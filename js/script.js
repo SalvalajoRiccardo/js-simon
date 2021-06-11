97d741a69ad91d078bc54da7cbd8e822f8ee7b5f
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-06-11 16:14:35 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@ var poolrandom = []
 var risposte = []
 var giuste = []
 var risultato = 0;
-
-for (var i = 0; i < 5; i++) {
+// generazione numeri random
+while (poolrandom.length < 5) {
     var num = getRandom(1,99);
+    // bonus
     if(!poolrandom.includes(num)) {
         poolrandom.push(num)
-    }
-    
+    } 
 }
 console.log(poolrandom);
-
+// funzione bottone
 document.getElementById('parti').addEventListener('click',function(){
     alert ('numeri da ricordare: ' + poolrandom)
     setTimeout(fine,30000)
@@ -20,9 +20,10 @@ document.getElementById('parti').addEventListener('click',function(){
     
 function fine () {
     for (i = 0; i < 5; i++){
-        var risp = parseInt(prompt('inserisci i 5 numeri che ricordi ma 1 alla volta'))
+        var risp = parseInt(prompt('inserisci i 5 numeri che ricordi, 1 alla volta'))
+        // bonus
         while (isNaN(risp) || risposte.includes(risp)){
-            risp = parseInt(prompt('ATTENZIONE, inserisci i NUMERI che ricordi, 1 alla volta'))
+            risp = parseInt(prompt('ATTENZIONE, inserisci i NUMERI che ricordi, ma 1 alla volta'))
         }  
 
         if (poolrandom.includes(risp)) {
@@ -40,7 +41,7 @@ function fine () {
 
 
 
-
+// Funzioni
 function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
